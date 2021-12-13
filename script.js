f51dbc66face5134fa017e92806b01f890dc18d6
UncleBikki <nickcummings1994@gmail.com>
2021-12-12 22:02:34 -0600

    added pseudo code

---
+++ script.js
@@ -0,0 +1,9 @@
+//i need variables  
+//i need to access api from open weather
+//i need function to return the lat and long in the api
+//i need a function that displays the date with a for loop
+//i need to save searched cities in local storage
+//i need to use local storage function to load cities called upon
+// i need to create buttons that can be used to load previous searches
+//i need an event handler function with prevent default 
+//
